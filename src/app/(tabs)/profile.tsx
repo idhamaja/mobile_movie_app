de6012323c33342d07764c88148fb3abcda46864
Idham Ganteng <idham16iramap@gmail.com>
2026-07-06 19:31:29 +0700

    update now today 6 July 2026

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from "react-native";
+import { icons } from "../../../constants/icons";
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.person} className="size-10" tintColor="#FFF" />
+        <Text className="text-gray-500 text-base">Profile Screen</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default profile
+export default profile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/app/(tabs)/saved.tsx
+++ src/app/(tabs)/saved.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { icons } from "../../../constants/icons";
 
 const saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.save} className="size-10" tintColor="#FFF" />
+        <Text className="text-gray-500 text-base">Saved Screen</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default saved
+export default saved;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
